db20a20c405777e8ed5e7e604d85db3f50ae54fa
travtrego <103004197+travtrego@users.noreply.github.com>
2026-08-16 08:51:08 -0400

    feat(cards): export Mega Evolution set

--- packages/sets/src/standard/index.ts
+++ packages/sets/src/standard/index.ts
@@ -4,5 +4,6 @@ export * from './set-black-and-white-3';
 export * from './set-black-and-white-4';
 export * from './set-diamond-and-pearl';
 export * from './set-hgss';
+export * from './set-mega-evolution';
 export * from './set-op9';
 export * from './set-sword-and-shield';
