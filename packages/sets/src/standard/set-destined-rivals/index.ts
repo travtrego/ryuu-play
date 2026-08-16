70b934afea209bb7416951b9350a5f034367893d
travtrego <103004197+travtrego@users.noreply.github.com>
2026-08-16 09:12:21 -0400

    feat(cards): register Team Rocket's Mewtwo ex

--- packages/sets/src/standard/set-destined-rivals/index.ts
+++ packages/sets/src/standard/set-destined-rivals/index.ts
@@ -2,6 +2,7 @@ import { Card } from '@ptcg/common';
 import { TeamRocketsAriana } from './team-rockets-ariana';
 import { TeamRocketsFactory } from './team-rockets-factory';
 import { TeamRocketsGiovanni } from './team-rockets-giovanni';
+import { TeamRocketsMewtwoEx } from './team-rockets-mewtwo-ex';
 import { TeamRocketsPetrel } from './team-rockets-petrel';
 import { TeamRocketsSpidops } from './team-rockets-spidops';
 import { TeamRocketsTarountula } from './team-rockets-tarountula';
@@ -11,6 +12,7 @@ export const setDestinedRivals: Card[] = [
   new TeamRocketsAriana(),
   new TeamRocketsFactory(),
   new TeamRocketsGiovanni(),
+  new TeamRocketsMewtwoEx(),
   new TeamRocketsPetrel(),
   new TeamRocketsSpidops(),
   new TeamRocketsTarountula(),
