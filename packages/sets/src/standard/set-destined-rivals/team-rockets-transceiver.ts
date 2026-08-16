fd6f8b3d44b016cd23f155c7c5668eee9b953afe
travtrego <103004197+travtrego@users.noreply.github.com>
2026-08-16 09:06:11 -0400

    feat(cards): implement Team Rocket's Transceiver

---
+++ packages/sets/src/standard/set-destined-rivals/team-rockets-transceiver.ts
@@ -0,0 +1,90 @@
+import {
+  Card,
+  CardList,
+  ChooseCardsPrompt,
+  Effect,
+  GameError,
+  GameMessage,
+  ShowCardsPrompt,
+  ShuffleDeckPrompt,
+  State,
+  StateUtils,
+  StoreLike,
+  TrainerCard,
+  TrainerEffect,
+  TrainerType,
+} from '@ptcg/common';
+import { isTeamRocketSupporter } from './team-rocket-utils';
+
+function* playCard(
+  next: Function,
+  store: StoreLike,
+  state: State,
+  effect: TrainerEffect
+): IterableIterator<State> {
+  const player = effect.player;
+  const opponent = StateUtils.getOpponent(state, player);
+  const eligible = player.deck.cards.filter(card =>
+    card instanceof TrainerCard && isTeamRocketSupporter(card)
+  );
+  let cards: Card[] = [];
+
+  if (eligible.length === 0) {
+    throw new GameError(GameMessage.CANNOT_PLAY_THIS_CARD);
+  }
+
+  const searchPool = new CardList();
+  searchPool.cards = eligible;
+
+  yield store.prompt(
+    state,
+    new ChooseCardsPrompt(
+      player.id,
+      GameMessage.CHOOSE_CARD_TO_HAND,
+      searchPool,
+      {},
+      { min: 1, max: 1, allowCancel: false }
+    ),
+    selected => {
+      cards = selected || [];
+      next();
+    }
+  );
+
+  if (cards.length > 0) {
+    yield store.prompt(
+      state,
+      new ShowCardsPrompt(opponent.id, GameMessage.CARDS_SHOWED_BY_THE_OPPONENT, cards),
+      () => next()
+    );
+  }
+
+  player.deck.moveCardsTo(cards, player.hand);
+
+  return store.prompt(state, new ShuffleDeckPrompt(player.id), order => {
+    player.deck.applyOrder(order);
+  });
+}
+
+export class TeamRocketsTransceiver extends TrainerCard {
+  public trainerType: TrainerType = TrainerType.ITEM;
+
+  public set: string = 'DRI';
+
+  public name: string = 'Team Rocket\'s Transceiver';
+
+  public fullName: string = 'Team Rocket\'s Transceiver DRI';
+
+  public text: string =
+    'Search your deck for a Supporter card that has "Team Rocket" in its name, reveal it, and put it into your hand. ' +
+    'Then, shuffle your deck.';
+
+  public reduceEffect(store: StoreLike, state: State, effect: Effect): State {
+    if (effect instanceof TrainerEffect && effect.trainerCard === this) {
+      const generator = playCard(() => generator.next(), store, state, effect);
+      return generator.next().value;
+    }
+
+    return state;
+  }
+}
